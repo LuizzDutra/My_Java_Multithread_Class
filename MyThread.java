5fd525d06ec533754f18766e6ccd212b44f3807a
LuizzDutra <luizzidutra@gmail.com>
2023-04-08 17:40:48 -0300

    MyThread Class

---
+++ MyThread.java
@@ -0,0 +1,39 @@
+package CalculatingPi;
+
+import java.util.ArrayList;
+
+public class MyThread extends Thread
+{
+
+    private Object result;
+
+
+    threadFace targetFunction;
+    //Example
+    //thread.targetFunction = () -> yourNonVoidFunction();
+    //thread.targetFunction = () -> {yourVoidFunction(); return null;};
+
+    public void run(){
+        result = targetFunction.run();
+    }
+
+    public Object getResult(){
+        return result;
+    }
+
+    public static boolean checkThreadStatus(ArrayList<MyThread> threadArray){
+        boolean resultBool = false;
+        for(MyThread thread : threadArray){
+            if(thread.isAlive()){resultBool = true; break;}
+        }
+        return resultBool;
+    }
+
+    interface threadFace
+    {
+        Object run();
+    }
+
+
+}
+
